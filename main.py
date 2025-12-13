73f510f3cdef12c0218174e8bc6b1c4e93a70d43
Afsan <idrisiafsan55@gmail.com>
2025-12-13 09:10:33 +0530

    presentation

--- main.py
+++ main.py
@@ -22,6 +22,5 @@ animate_earth(canvas, S_WIDTH, S_HEIGHT, assets, root)
 rotate_radar(canvas, S_WIDTH, S_HEIGHT, 0, assets, root)
 spawn_asteroid(canvas, asteroids, assets, root)
 updateAsteroids(canvas, asteroids, root)
-# update_status(canvas, asteroids, assets, root)
 
 root.mainloop()
